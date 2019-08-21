4a0f1bcd7c31280cbdfd7c26d4840ac9d41aebb1
tatendachakwesha <tchakweshashscul@gmail.com>
2019-08-21 20:43:09 +0200

    Updates

---
+++ precache-manifest.1ea17a143ab5dc65edc8d33e714db6b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6086ce0501adfdb0457dc46b1d594db8",
+    "url": "/normalsite/index.html"
+  },
+  {
+    "revision": "52cd63cf68256eee3a2a",
+    "url": "/normalsite/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "9ab8f7fe3ed710873459",
+    "url": "/normalsite/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "52cd63cf68256eee3a2a",
+    "url": "/normalsite/static/js/2.e74200e1.chunk.js"
+  },
+  {
+    "revision": "9ab8f7fe3ed710873459",
+    "url": "/normalsite/static/js/main.6dbe5187.chunk.js"
+  },
+  {
+    "revision": "e18dad019cc78a4d82bd",
+    "url": "/normalsite/static/js/runtime~main.0c94bfe5.js"
+  },
+  {
+    "revision": "1d0f9a39e4238045962f55ff6b8c62f3",
+    "url": "/normalsite/static/media/logo.1d0f9a39.png"
+  }
+]);
